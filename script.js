32d39b37ae7288752fdb082e67b33c78d9f8f9e7
Derricksyanokimani <derricksyanokimanistudy@proton.me>
2026-04-06 17:23:15 +0300

    Added click event and input reading.

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const addBtn = document.getElementById('addBtn');
 const taskInput = document.getElementById('taskInput');
 const taskList = document.getElementById('taskList');
 
-console.log(addBtn);
 
 addBtn.addEventListener("click", function()
     {
